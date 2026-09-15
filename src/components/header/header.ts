52750f80426067a010005635598cbb9cbddf9777
Routervx1 <voum00@gmail.com>
2026-09-14 20:34:48 -0700

    Add files via upload

---
+++ src/components/header/header.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Header {
+  private readonly cartService = inject(CartService);
+  readonly itemCount = this.cartService.itemCount;
+}
